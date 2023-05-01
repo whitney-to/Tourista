a7b2694b1c15431cf5b5d800573484c3f790f522
whitney-to <whitney012923@gmail.com>
2023-05-01 09:25:29 -0400

    added backend folder

---
+++ frontend/src/main/java/com/wanderbuddy/tourists/TouristsApplication.java
@@ -0,0 +1,13 @@
+package com.wanderbuddy.tourists;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouristsApplication.class, args);
+	}
+
+}
