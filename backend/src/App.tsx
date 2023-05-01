4f664eb3b38ac0d11238d3d16fe897de5285041e
whitney-to <whitney012923@gmail.com>
2023-04-30 22:21:38 -0400

    added react for backend

---
+++ backend/src/App.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { Row } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+      <Row>
+        <Header></Header>
+
+      </Row>
+      <Row>
+        <Footer></Footer>
+
+      </Row>
+    </div>
+  );
+}
+
+export default App;
